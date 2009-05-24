06b2faca4e1bd000623cd368520070a4e2a161aa
robert-wm <robert-wm@cab531d2-9084-44d6-b46b-e60b69cb9836>
2009-05-24 14:40:23 +0000

    added cu boot wrapper for AB IPBox 200/250/350/400 added device tree for AB IPBox 350 added platform AB IPBox 400
    git-svn-id: svn://svn.code.sf.net/p/stbx25xx-linux/code@61 cab531d2-9084-44d6-b46b-e60b69cb9836

---
+++ arch/powerpc/boot/cuboot-ip400.c
@@ -0,0 +1,60 @@
+/*
+ * arch/powerpc/boot/cuboot-ip400.c
+ * 
+ * Old U-boot compatibility for AB IPBox 400 
+ *
+ * Author: (C) Robert Burger <robert_burger@web.de>
+ *
+ * This file is licensed under the terms of the GNU General Public License 
+ * version 2. This file is licensed "as is" without any warranty of any 
+ * kind, whether express or implied.
+ */
+
+#include "ops.h"
+#include "io.h"
+#include "dcr.h"
+#include "stdio.h"
+#include "4xx.h"
+#include "cuboot.h"
+
+typedef struct bd_info {
+/* Legacy data from the old bd_info struct */
+  unsigned char bi_s_version[4];    /* Version of this structure */
+  unsigned char bi_r_version[30];   /* Version of the IBM ROM */
+  unsigned int  bi_memsize_legacy;  /* DRAM installed, in bytes */
+  unsigned int  bi_dummy;           /* field shouldn't exist */
+  unsigned char bi_enetaddr[6];     /* Ethernet MAC address */
+  unsigned int  bi_intfreq;         /* Processor speed, in Hz */
+  unsigned int  bi_busfreq;         /* Bus speed, in Hz */
+  unsigned int  bi_tbfreq;          /* Software timebase freq */
+  unsigned int  bi_opbfreq;         /* OPB Bus speed, in Hz */
+  int bi_iic_fast[2];               /* Use fast i2c mode */
+/* End of legacy data */
+  unsigned long bi_memstart;
+  unsigned long bi_memsize;
+  unsigned long bi_flashstart;  /* start of FLASH memory */
+  unsigned long bi_flashsize;   /* size  of FLASH memory */
+  unsigned long bi_flashoffset; /* reserved area for startup monitor */
+  unsigned long bi_sramstart;   /* start of SRAM memory */
+  unsigned long bi_sramsize;    /* size  of SRAM memory */
+  unsigned long bi_baudrate;    /* Console Baudrate */
+  unsigned int  bi_procfreq;    /* Processor speed, in Hz */
+  unsigned long bi_bootflags;   /* boot / reboot flag (for LynxOS) */
+  unsigned int  bi_plb_busfreq; /* PLB Bus speed, in Hz */
+  unsigned long bi_ip_addr;     /* IP Address */
+} bd_t;
+static bd_t bd;
+
+static void ip400_fixups(void)
+{
+}
+	
+void platform_init(unsigned long r3, unsigned long r4, unsigned long r5,
+		unsigned long r6, unsigned long r7)
+{
+	CUBOOT_INIT();
+	platform_ops.fixups = ip400_fixups;
+	platform_ops.exit = ibm40x_dbcr_reset;
+	fdt_init(_dtb_start);
+	serial_console_init();
+}
